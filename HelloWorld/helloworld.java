e3fc8fda3c01c41bcf7b91fb9d284f107879673e
TEJENDRA <TEJENDRA@10.85.109.59>
2015-04-15 13:17:07 -0700

    final commit

--- HelloWorld/helloworld.java
+++ HelloWorld/helloworld.java
@@ -3,7 +3,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello !!   ff  everyone");
+System.out.println("hello !!   everyone");
 	}
 
 }
